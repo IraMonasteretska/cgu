c020cf5a4fdb99a24f0544a5c296ea485407e6ff
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-10-08 17:26:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -75,6 +75,15 @@ $(document).ready(function () {
         });
 
     }
+    // selects main page
+    if ($('select').length) {
+        $('.styledselect').select2({
+            placeholder: "",
+            minimumResultsForSearch: Infinity,
+        });
+
+        $('#sort').select2({ dropdownAutoWidth: true, minimumResultsForSearch: Infinity, });
+    }
 
     // show modification tool
     $('.explain-btn').click(function () {
@@ -85,6 +94,158 @@ $(document).ready(function () {
         $('.uploadfromtel').addClass('hide');
         $('.explain-box').show();
     })
+
+
+
+
+    // enable tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    $('[data-bs-toggle="tooltip"]').tooltip({
+        trigger: 'hover'
+    });
+
+    // show notifications
+    $('.action--alerts').click(function () {
+        $('.alerts-dropdown').toggleClass('active');
+    })
+
+    $('.alerts-dropdown__close').click(function () {
+        $('.alerts-dropdown').removeClass('active');
+    })
+
+    $('.show-alerts__btn').click(function () {
+        $(this).parents('.alerts__box').find('.alerts-wrap').toggle();
+    })
+
+    $(document).click(function (event) {
+        let $target = $(event.target);
+        if (!$target.closest('.alerts-dropdown').length && !$target.closest('.action--alerts').length) {
+            $('.alerts-dropdown').removeClass('active');
+        }
+    });
+
+    // Layout Elements popup
+    $('.layout-result .close-button').click(function () {
+        $('.layout-result').addClass('hide');
+    })
+    $('.show-result').click(function () {
+        $('.layout-result').removeClass('hide');
+    })
+
+    $('.plcreate').click(function () {
+        $('.planlayout-modal ').fadeIn();
+        $('.planlayout-overlay').fadeIn();
+    });
+    $('.close-layout').click(function () {
+        $('.planlayout-modal ').fadeOut();
+        $('.planlayout-overlay').fadeOut();
+    })
+
+
+
+
+    // filter 
+    // $('.filter-btn').click(function () {
+    //     $('.plans-bodysect').toggleClass('filter-hide');
+    //     $('.showfilter').toggleClass('show');
+
+    //     let $extraBlock = $('#extra-block');
+
+    //     if ($extraBlock.length) {
+    //         $extraBlock.remove();
+    //     } else {
+    //         $('body').append('<div id="extra-block" class="extra-block"></div>');
+    //     }
+    // });
+
+    $(document).on('click', '#extra-block', function () {
+        $(this).remove();
+        $('.plans-bodysect').toggleClass('filter-hide');
+        $('.showfilter').toggleClass('show');
+    });
+
+
+    $('.filter-box__title').click(function () {
+        $(this).toggleClass('open');
+        $(this).next('.filter-box__body').slideToggle();
+    });
+
+    $('.close-filter').click(function () {
+        $('#extra-block').remove();
+        $('.plans-bodysect').toggleClass('filter-hide');
+    });
+
+
+    // Range slider
+    if ($('.js-range-slider').length) {
+        $(".js-range-slider").ionRangeSlider({
+            type: "double",
+            min: 0,
+            max: 500000,
+            from: 10000,
+            to: 300000,
+            skin: 'round',
+            hide_from_to: true,
+            hide_min_max: true,
+
+            onChange: function (data) {
+                $('#slider-input-from').val(data.from);
+                $('#slider-input-to').val(data.to);
+            },
+        });
+    }
+
+    $('.range-slider-input').change(function () {
+        let inputId = $(this).attr('id');
+        let inputValue = $(this).val();
+
+        let my_range = $(".js-range-slider").data("ionRangeSlider");
+
+        if (inputId === 'slider-input-from') {
+            my_range.update({
+                from: inputValue
+            });
+        } else {
+            my_range.update({
+                to: inputValue
+            });
+        }
+    });
+
+    // Filter - more btn
+    $('.fiter-more_btn').click(function () {
+        $(this).toggleClass('active');
+        $(this).parent('.checkboxes-item').toggleClass('active');
+    });
+
+    // Layout Elements
+    $('.elements-title').click(function () {
+        $('.elements-title').addClass('hide-box');
+        $(this).removeClass('hide-box');
+
+        $('.elements-box').addClass('hide');
+        $(this).next($('.elements-box')).removeClass('hide');
+    });
+
+    $(".js-range-slider2").ionRangeSlider({
+        type: "single",
+        min: 0,
+        max: 100,
+        skin: 'round',
+        hide_from_to: true,
+        hide_min_max: true,
+    });
+
+    // toggle view plans
+    $('.toggleviiew__btn').click(function () {
+        $('.toggleviiew__btn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+
 
 
 
